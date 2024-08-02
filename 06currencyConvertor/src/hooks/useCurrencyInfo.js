72e34ff143ad556d4b67318142eedc4c9303161e
himanshu <himanshumeena2416@gmail.com>
2024-08-02 14:23:28 +0530

    currencyConvertor

---
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,18 @@
+//yaha hum humara custom hook bana rhe hai.
+//hook bhi ek function hi hota hai
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+  }, [currency]);
+
+  return data;
+}
+
+export default useCurrencyInfo;
